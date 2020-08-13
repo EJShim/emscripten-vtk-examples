de0181210efdff5c092f887756c9ab69318512c4
ejshim@imagoworks.ai <soulrommel@gmail.com>
2020-08-13 14:36:22 +0900

    temp

--- Picking/Picking.cxx
+++ Picking/Picking.cxx
@@ -9,7 +9,8 @@
 #include "vtkRenderer.h"
 #include <vtkCylinderSource.h>
 #include <vtkPropPicker.h>
-
+#include <vtkCamera.h>
+#include <vtkCoordinate.h>
 
 
 #ifdef EMSCRIPTEN
@@ -26,49 +27,54 @@
 // Handle mouse events
 class MouseInteractorStyle2 : public vtkInteractorStyleTrackballCamera
 {
-  public:
-    static MouseInteractorStyle2* New();
-    vtkTypeMacro(MouseInteractorStyle2, vtkInteractorStyleTrackballCamera);
+public:
+	static MouseInteractorStyle2* New();
+	vtkTypeMacro(MouseInteractorStyle2, vtkInteractorStyleTrackballCamera);
 
-    virtual void OnLeftButtonDown() override
-    {
-      int* clickPos = this->GetInteractor()->GetEventPosition();
+	virtual void OnLeftButtonDown() override
+	{
+		int* clickPos = this->GetInteractor()->GetEventPosition();
 
-
-      std::cout << "clicked Position : " << ":" << clickPos[0] << "," << clickPos[1] << std::endl;
-      vtkRenderer* renderer = this->GetDefaultRenderer();
+		vtkSmartPointer<vtkCoordinate> coordinate = vtkSmartPointer<vtkCoordinate>::New();
 
 
-      // Pick from this location.
-      vtkSmartPointer<vtkPropPicker>  picker = vtkSmartPointer<vtkPropPicker>::New();
-      picker->Pick(clickPos[0], clickPos[1], 0, renderer);
-
-      // double* pos = picker->GetPickPosition();
-      // std::cout << "Pick position (world coordinates) is: " << pos[0] << " " << pos[1] << " " << pos[2] << std::endl;
-      // std::cout << "Picked actor: " << picker->GetActor() << std::endl;
-
-      // std::cout << "Picked actor: " << picker->GetActor() << std::endl;
-      // //Create a sphere
-      // vtkSmartPointer<vtkSphereSource> sphereSource = vtkSmartPointer<vtkSphereSource>::New();
-      // sphereSource->SetCenter(pos[0], pos[1], pos[2]);
-      // sphereSource->SetRadius(0.1);
-
-      // //Create a mapper and actor
-      // vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-      // mapper->SetInputConnection(sphereSource->GetOutputPort());
-
-      // vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
-      // actor->SetMapper(mapper);
+		int displayPos[3] = {clickPos[0], clickPos[1], 0};
 
 
-      // //this->GetInteractor()->GetRenderWindow()->GetRenderers()->GetDefaultRenderer()->AddActor(actor);
-      // this->GetDefaultRenderer()->AddActor(actor);
-      // Forward events
-      vtkInteractorStyleTrackballCamera::OnLeftButtonDown();
-    }
 
-  private:
+		vtkRenderer* renderer = this->GetDefaultRenderer();
 
+
+		//Compute display to world
+
+
+
+
+
+		// std::cout << "Picked actor: " << picker->GetActor() << std::endl;
+
+		// std::cout << "Picked actor: " << picker->GetActor() << std::endl;
+		// //Create a sphere
+		// vtkSmartPointer<vtkSphereSource> sphereSource = vtkSmartPointer<vtkSphereSource>::New();
+		// sphereSource->SetCenter(pos[0], pos[1], pos[2]);
+		// sphereSource->SetRadius(0.1);
+
+		// //Create a mapper and actor
+		// vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+		// mapper->SetInputConnection(sphereSource->GetOutputPort());
+
+		// vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+		// actor->SetMapper(mapper);
+
+
+		// //this->GetInteractor()->GetRenderWindow()->GetRenderers()->GetDefaultRenderer()->AddActor(actor);
+		// this->GetDefaultRenderer()->AddActor(actor);
+		// Forward events
+		vtkInteractorStyleTrackballCamera::OnLeftButtonDown();
+	}
+  
+private:
+  
 };
 
 vtkStandardNewMacro(MouseInteractorStyle2);
@@ -92,53 +98,53 @@ static vtkRenderWindow* renderWindow = vtkRenderWindow::New();
 
 int main(int argc, char* argv[])
 {
-  // Create a renderer and interactor
-  vtkNew<vtkRenderer> renderer;
-  renderWindow->AddRenderer(renderer);
+	// Create a renderer and interactor
+	vtkNew<vtkRenderer> renderer;
+	renderWindow->AddRenderer(renderer);
 
-  #ifdef EMSCRIPTEN
-    vtkNew<vtkSDL2RenderWindowInteractor> renderWindowInteractor;
-  #else
-    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  #endif
+	#ifdef EMSCRIPTEN
+	vtkNew<vtkSDL2RenderWindowInteractor> renderWindowInteractor;
+	#else
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	#endif
 
-  renderWindowInteractor->SetRenderWindow(renderWindow);
-  vtkSmartPointer<MouseInteractorStyle2> style = vtkSmartPointer<MouseInteractorStyle2>::New();
-  renderWindowInteractor->SetInteractorStyle(style);
-  style->SetDefaultRenderer(renderer);
+	renderWindowInteractor->SetRenderWindow(renderWindow);
+	vtkSmartPointer<MouseInteractorStyle2> style = vtkSmartPointer<MouseInteractorStyle2>::New();
+	renderWindowInteractor->SetInteractorStyle(style);
+	style->SetDefaultRenderer(renderer);
 
-  // Create pipeline
-  vtkNew<vtkConeSource> coneSource;
-  coneSource->Update();
+	// Create pipeline
+	vtkNew<vtkConeSource> coneSource;
+	coneSource->Update();
 
-    // Create a sphere
-    vtkSmartPointer<vtkCylinderSource> cylinderSource =
-    vtkSmartPointer<vtkCylinderSource>::New();
-    cylinderSource->SetCenter(0.0, 0.0, 0.0);
-    cylinderSource->SetRadius(5.0);
-    cylinderSource->SetHeight(7.0);
-    cylinderSource->SetResolution(100);
+	// Create a sphere
+	vtkSmartPointer<vtkCylinderSource> cylinderSource =
+	vtkSmartPointer<vtkCylinderSource>::New();
+	cylinderSource->SetCenter(0.0, 0.0, 0.0);
+	cylinderSource->SetRadius(5.0);
+	cylinderSource->SetHeight(7.0);
+	cylinderSource->SetResolution(100);
 
-  vtkNew<vtkOpenGLPolyDataMapper> mapper;  
-  mapper->SetInputConnection(cylinderSource->GetOutputPort());
+	vtkNew<vtkOpenGLPolyDataMapper> mapper;  
+	mapper->SetInputConnection(cylinderSource->GetOutputPort());
 
-  vtkNew<vtkActor> actor;
-  actor->SetMapper(mapper);
-
-  
-  std::cout << "Actor address: " << actor << std::endl;
-
-  // Add the actors to the scene
-  renderer->AddActor(actor);
-
-  // Start rendering app
-  renderer->SetBackground(.1, .3, .2);
+	vtkNew<vtkActor> actor;
+	actor->SetMapper(mapper);
 
 
-  // Start event loop
-  renderWindowInteractor->Start();
+	std::cout << "Actor address: " << actor << std::endl;
 
-  // Exit
-  renderWindow->Delete();
-  return 0;
+	// Add the actors to the scene
+	renderer->AddActor(actor);
+
+	// Start rendering app
+	renderer->SetBackground(.1, .3, .2);
+
+
+	// Start event loop
+	renderWindowInteractor->Start();
+
+	// Exit
+	renderWindow->Delete();
+	return 0;
 }
